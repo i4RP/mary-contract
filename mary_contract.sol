6b7c776c865cb812536748a91143399db2bdff11
ダルビッシュ <shoei0205@gmail.com>
2018-07-24 03:06:17 +0900

    ver1

--- mary_contract.sol
+++ mary_contract.sol
@@ -1,6 +1,7 @@
 pragma solidity ^0.4.19;
 
-
-function propose(address _address) external payable {
-    _address.transfer(msg.value)
+contract Mary {
+    function propose(address _address) external payable {
+    _address.transfer(msg.value);
+    }
 }
